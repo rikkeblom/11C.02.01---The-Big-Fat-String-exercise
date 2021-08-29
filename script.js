528c02f8a26f8cbdc066bbbe2761e1a618ef2354
Rikke Blom <rikk957v@stud.kea.dk>
2021-08-29 22:33:26 +0200

    HTML structure and plan for js script

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//MY PLAN-----------------------------------------------------
+//set eventlistener on button
+//find value of the dropdown to know what to do to the string
+//call the according function
+//read input from textfield
+//edit string
+//print edited string in output
+//------------------------------------------------------------
